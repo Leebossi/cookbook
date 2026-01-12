30013603d9f8debdea0f37f916b4f93938e24020
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2026-01-12 15:06:11 +0200

    database connected

--- index.ts
+++ index.ts
@@ -1,15 +1,23 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import { Sequelize, QueryTypes } from "sequelize";
+
+dotenv.config();
 const app = express();
+const sequelize = new Sequelize(process.env.DATABASE_URL || "");
 
-app.get('/', (_req, res) => {
-  res.send('Hello World!');
+app.get("/", (_req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/ping', (_req, res) => {
-  res.send('pong');
+app.get("/recipe", async(_req, res) => {
+  const recipes = await sequelize.query("SELECT * FROM recipe;", {
+    type: QueryTypes.SELECT,
+  });
+  res.json(recipes);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
