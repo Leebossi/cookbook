bc0948dda0ebbacec5dd08627f9fb95bc331345a
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2026-02-03 13:58:02 +0200

    instructions and ingredients router

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ import express from "express";
 import config from "./util/config";
 import connectToDatabase from "./util/db";
 import recipeRouter from "./controllers/recipes";
+import ingredientRouter from "./controllers/ingredients";
+import instructionRouter from "./controllers/instructions";
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/recipes", recipeRouter);
+app.use("/api/ingredients", ingredientRouter);
+app.use("/api/instructions", instructionRouter);
 
 const startServer = async () => {
   await connectToDatabase();
